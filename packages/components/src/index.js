a8f2d0df4d33a53d5fa51d069a5869f595f7fdd4
Gauca <gaucab@gb-locs-MacBook-Pro.local>
2020-05-10 11:58:08 +0300

    Publish
     - @demo-ui-toolkit/components@1.0.7
     - @demo-ui-toolkit/mui-themes@0.0.6

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from "./components/Button/Button";
 export { default as MyButton } from "./components/MyButton/MyButton";
+export { default as CustomComponent } from "./components/CustomComponent/CustomComponent";
