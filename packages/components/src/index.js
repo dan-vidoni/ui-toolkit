b8497d76b03bcd4f1d1977a7d9278322bcdc0ed3
dan vidoni <vidonid@haufe.com>
2020-05-10 22:30:34 +0300

    fix'

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,3 +1,3 @@
 export { default as Button } from "./components/Button/Button";
-export { default as MyButton } from "./components/MyButton/MyButton";
+export { default as MyStyledButton } from "./components/MyStyledButton/MyStyledButton";
 export { default as CustomComponent } from "./components/CustomComponent/CustomComponent";
