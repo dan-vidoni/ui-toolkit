86bb8ccd0b1d8c6282468d52a798d5c95bc31fb2
dan vidoni <vidonid@haufe.com>
2020-05-12 16:37:19 +0300

    add exports

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,4 +1,11 @@
 export { default as Button } from "./Button";
+export * from "./Button";
+
 export { default as Label } from "./Label";
+export * from "./Label";
+
 export { default as InputText } from "./InputText";
+export * from "./InputText";
+
 export { default as Card } from "./Card";
+export * from "./Card";
