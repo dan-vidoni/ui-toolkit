83fdc2fa255f5ec4a6079d346d956619be459390
Gauca <gaucab@gb-locs-MacBook-Pro.local>
2020-05-13 11:40:26 +0300

    new component

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Label } from "./Label";
 export { default as InputText } from "./InputText";
 export { default as Card } from "./Card";
+export { default as ChipsList } from "./ChipsList";
